edb4204429823d9107768b37421eb7f1a77a5575
Derek Nielsen <162534561+DerekNielsenCSWB110@users.noreply.github.com>
2025-11-12 00:39:52 -0800

    js for hamburger was still in html

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,3 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const lastModified = document.lastModified;
   document.getElementById("lastModified").textContent = lastModified;
 });
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
